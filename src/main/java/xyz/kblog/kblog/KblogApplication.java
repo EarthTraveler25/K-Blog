41fca534fab0eba40f6e1b14afcd7407aed3cb3f
GHY12 <earthtraveler2025@gmail.com>
2025-12-11 00:17:16 +0900

    Add Spring Boot project structure

---
+++ src/main/java/xyz/kblog/kblog/KblogApplication.java
@@ -0,0 +1,13 @@
+package xyz.kblog.kblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KblogApplication.class, args);
+	}
+
+}
